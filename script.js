6fcac4795a08a1f681c36ea86c983fde34530d0e
spacegaming631 <123908053+sg631@users.noreply.github.com>
2024-03-07 20:46:38 +0000

    Changed Button style

--- script.js
+++ script.js
@@ -15,6 +15,15 @@ p.sgm {
 }
 button.sgm {
   border: 3px solid white; padding: 10px;
+  background-color: rgb(40, 40, 40)
+  color: white;
+  cursor: pointer
+}
+button.sgm:hover {
+  background-color: rgb(50, 50, 50)
+}
+button.sgm:active {
+  background-color: rgb(20, 20, 20)
 }
 </style><div class="sgm" id="menu" style="border-radius: 0px 0px 0px 40px; position: fixed; top: 0; right: 0; width: 300px; height: 300px; background-color: rgba(30, 30, 30, 1); z-index: 9999;border: 3px solid white; padding: 10px; display: flex; flex-direction: column; align-items: center; justify-content: center; display:none; animation-duration:1s;"><p class="sgm">Extension Menu</p><button class="sgm">Open in A:B</button></div>`
 document.body.appendChild(menucontainer);
