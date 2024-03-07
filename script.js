13c00154ad4e83f0a5db5f7a3fd5ee41cb0238d2
spacegaming631 <123908053+sg631@users.noreply.github.com>
2024-03-07 19:49:05 +0000

    Added corner rounding

--- script.js
+++ script.js
@@ -8,12 +8,12 @@ menucontainer.innerHTML = `<style>@keyframes close {
   from {opacity:0}
   to {opacity:1}
 }
-html {
+p {
   color:white;
   font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
   text-align: center;
 }
-</style><div id="menu" style="position: fixed; top: 0; right: 0; width: 300px; height: 300px; background-color: rgba(30, 30, 30, 1); z-index: 9999;border: 1px solid black; padding: 10px; display: flex; flex-direction: column; align-items: center; justify-content: center; display:none; animation-duration:1s;"><p>Extension Menu</p></div>`
+</style><div id="menu" style="border-radius: 0px 0px 0px 40px; position: fixed; top: 0; right: 0; width: 300px; height: 300px; background-color: rgba(30, 30, 30, 1); z-index: 9999;border: 3px solid white; padding: 10px; display: flex; flex-direction: column; align-items: center; justify-content: center; display:none; animation-duration:1s;"><p>Extension Menu</p><button>Open in A:B</button></div>`
 document.body.appendChild(menucontainer);
 const menu = document.getElementById("menu")
 
