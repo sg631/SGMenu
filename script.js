805bff283d9005c86751afbf0969acdfdb760969
spacegaming631 <123908053+sg631@users.noreply.github.com>
2024-03-08 07:52:00 +0000

    Updated style

--- script.js
+++ script.js
@@ -18,45 +18,45 @@ p.sgm {
   display: none;
 }
 .sgm#sgmexec {
-  -ms-overflow-style: none;  /* IE and Edge */
-  scrollbar-width: none;  /* Firefox */
+  -ms-overflow-style: none !important;
+  scrollbar-width: none !important;
 }
 textarea.sgm {
-  outline:none;
-  resize:none;
-  border: 3px solid white; padding: 10px;
-  background-color: rgb(50, 50, 50);
-  color: white;
-  font-size:10pt;
-  height:20pt;
-  border-radius:25px;
-  margin: 0;
+  outline:none !important;
+  resize:none !important;
+  border: 3px solid white; padding: 10px !important;
+  background-color: rgb(50, 50, 50) !important;
+  color: white !important;
+  font-size:10pt !important;
+  height:20pt !important;
+  border-radius:25px !important;
+  margin: 0 !important;
 }
 button.sgm {
-  border: 3px solid white; padding: 10px;
-  background-color: rgb(50, 50, 50);
-  color: white;
-  cursor: pointer;
-  border-radius:25px;
-  margin: 0;
+  border: 3px solid white; padding: 10px !important;
+  background-color: rgb(50, 50, 50) !important;
+  color: white !important;
+  cursor: pointer !important;
+  border-radius:25px !important;
+  margin: 0 !important;
 }
 .sgmhorizcenter {
-  position: absolute;
-  left: 50%;
-  -ms-transform: translateX(-50%);
-  transform: translateX(-50%);
+  position: absolute !important;
+  left: 50% !important;
+  -ms-transform: translateX(-50%) !important;
+  transform: translateX(-50%) !important;
 }
 .sgmverticenter {
-  position: absolute;
-  top: 50%;
-  -ms-transform: translateX(-50%);
-  transform: translateX(-50%);
+  position: absolute !important;
+  top: 50% !important;
+  -ms-transform: translateX(-50%) !important;
+  transform: translateX(-50%) !important;
 }
 button.sgm:hover {
-  background-color: rgb(60, 60, 60)
+  background-color: rgb(60, 60, 60) !important;
 }
 button.sgm:active {
-  background-color: rgb(20, 20, 20)
+  background-color: rgb(20, 20, 20) !important;
 }
 </style><div class="sgm" id="menu" style="border-radius: 0px 0px 0px 40px; position: fixed; top: 0; right: 0; width: 300px; height: 100%; background-color: rgba(30, 30, 30, 1); z-index: 9999;border: 3px solid white; padding: 10px; display: flex; flex-direction: column; align-items: center; justify-content: center; display:none; animation-duration:0.2s;"><br><p class="sgm" style="font-size:20pt">SGMenu</p><button id="sgmab" class="sgm sgmhorizcenter">Open in A:B</button><br><br><p class="sgm">Execute Javascript</p><textarea id="sgmexec" class="sgm" style=""></textarea><button class="sgm" style="" id="sgmexecbutton" onclick="eval(sgm_exec.value)">Execute</button></div>`
 document.body.appendChild(menucontainer);
