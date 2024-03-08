c0a989bd424a20c2246ec0104aa71800733221c6
spacegaming631 <123908053+sg631@users.noreply.github.com>
2024-03-08 00:28:15 +0000

    Added Smooth menu close, shortened animation time, and centered button.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //menu setup
 const menucontainer = document.createElement("div");
-menucontainer.innerHTML = `<style>@keyframes close {
+menucontainer.innerHTML = `<style>
+@keyframes close {
   from {opacity:1}
   to {opacity:0}
 }
@@ -15,18 +16,23 @@ p.sgm {
 }
 button.sgm {
   border: 3px solid white; padding: 10px;
-  background-color: rgb(40, 40, 40);
+  background-color: rgb(50, 50, 50);
   color: white;
   cursor: pointer;
   border-radius:25px;
+  margin: 0;
+  position: absolute;
+  left: 50%;
+  -ms-transform: translateX(-50%);
+  transform: translateX(-50%);
 }
 button.sgm:hover {
-  background-color: rgb(50, 50, 50)
+  background-color: rgb(60, 60, 60)
 }
 button.sgm:active {
   background-color: rgb(20, 20, 20)
 }
-</style><div class="sgm" id="menu" style="border-radius: 0px 0px 0px 40px; position: fixed; top: 0; right: 0; width: 300px; height: 300px; background-color: rgba(30, 30, 30, 1); z-index: 9999;border: 3px solid white; padding: 10px; display: flex; flex-direction: column; align-items: center; justify-content: center; display:none; animation-duration:1s;"><p class="sgm">Extension Menu</p><button class="sgm">Open in A:B</button></div>`
+</style><div class="sgm" id="menu" style="border-radius: 0px 0px 0px 40px; position: fixed; top: 0; right: 0; width: 300px; height: 300px; background-color: rgba(30, 30, 30, 1); z-index: 9999;border: 3px solid white; padding: 10px; display: flex; flex-direction: column; align-items: center; justify-content: center; display:none; animation-duration:0.2s;"><p class="sgm">Extension Menu</p><button class="sgm">Open in A:B</button></div>`
 document.body.appendChild(menucontainer);
 const menu = document.getElementById("menu")
 
@@ -37,6 +43,7 @@ document.addEventListener('keydown', function (e) {
     //console.log(e.key);
     if (e.key === '~' && e.altKey) {
         menuOpenState = !menuOpenState;
+        setTimeout(function(){if(!menuOpenState){menu.style.display = "none"}}, 200)
     }
   if (menuOpenState){
     menu.style.animationName = "open"
@@ -45,7 +52,13 @@ document.addEventListener('keydown', function (e) {
 
   } else {
     menu.style.animationName = "close"
-    menu.style.display = "none"
+    
   }
     console.log(menuOpenState)
 });
+
+function customFunction(event) {
+  button.removeEventListener(transitionEvent, customFunction);
+
+  // Do something when the transition ends
+}
